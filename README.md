43f815775c8a870ea0eea72eac3f3b9ff8916cca
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-08-06 18:05:22 -0500

    initial function

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # appsync-athena-resolver
 
 # Environment Variables:
+- **AWS_ATHENA_REGION_NAME** REQUIRED - The region for Athena that this function should call
+- **AWS_ATHENA_S3_STAGING_DIR** REQUIRED - This is the location that Athena will store the query results in
+- **AWS_ATHENA_SCHEMA_NAME** OPTIONAL - If not provided, will default to the _default_ schema/database
 
 # Handler Method
 function.handler
--- src/lambda_function/function.py
+++ src/lambda_function/function.py
@@ -1,16 +1,48 @@
-import boto3
 import json
 import logging.config
 import os
 
+from pyathena import connect
 
+
+connection = connect(
+    region_name=os.environ['AWS_ATHENA_REGION_NAME'],
+    schema_name=os.environ.get('AWS_ATHENA_SCHEMA_NAME', 'default')
+)
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
+def map_result(description, row):
+    result = {}
+    for column in range(0, len(description)):
+        result[description[column][0]] = row[column]
+    return result
+
+
+def execute_query(query, single_result, params):
+    results = []
+    with connection.cursor() as cursor:
+        cursor.execute(query, params)
+        description = cursor.description
+        for row in cursor:
+            if single_result and len(results) == 1:
+                raise Exception(
+                    'Query {} returned more than one row, but result should be a single row'.format(cursor.query)
+                )
+            results.append(map_result(description, row))
+    logger.debug('Query returned {} results'.format(len(results)))
+    return results if not single_result else results[0] if len(results) else {}
+
+
 def handler(event, context):
-
     logger.debug('Processing event {}'.format(json.dumps(event)))
-
-
-    return event
+    if event['operation'] == 'Invoke':
+        result = execute_query(event['query'], event['single_result'], event.get('params'))
+    elif event['operation'] == 'BatchInvoke':
+        result = []
+        for sub_event in event:
+            result.append(execute_query(sub_event['query'], sub_event['single_result'], sub_event.get('params')))
+    else:
+        raise Exception('operation {} not supported'.format(event['operation']))
+    return result
