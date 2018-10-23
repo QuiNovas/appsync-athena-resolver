66c38be83e22f1351879ccded988b67eb148ac9a
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-10-23 18:33:19 -0500

    Rebuild to include
    newest version of lambda-pyathena

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ lambda_name = 'appsync-athena-resolver'
 # Versions should comply with PEP440.  For a discussion on single-sourcing
 # the version across setup.py and the project code, see
 # https://packaging.python.org/en/latest/single_source_version.html
-lambda_version = '0.0.1'
+lambda_version = '0.0.2'
 
 lambda_description = 'Lambda resolves to an Athena database for AppSync GraphQL'
 
