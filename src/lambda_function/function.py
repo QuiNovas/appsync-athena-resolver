50afea7db3c122e0d648cc3b5b20eb50638c2f6e
Joseph Wortmann <joseph.wortmann@gmail.com>
2020-01-28 16:50:32 -0600

    Cached connections by schemaName
    For warm processing speed

--- src/lambda_function/function.py
+++ src/lambda_function/function.py
@@ -9,42 +9,44 @@ from pyathena.async_cursor import AsyncCursor
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 MAX_CONCURRENT_QUERIES = int(os.environ.get('MAX_CONCURRENT_QUERIES', 5))
+CONNECTION_CACHE = {}
 
 
 def handler(event, context):
     logger.debug('Processing event {}'.format(json.dumps(event)))
-    with _connect(event.get('schemaName')) as connection:
-        if type(event) is dict:
-            logger.info('Processing Invoke operation')
-            with connection.cursor(max_workers=1) as cursor:
-                query_id, future = cursor.execute(event['query'].format(**event.get('params', {})))
-                # Default to multiple results if not present
-                result = _process_result_set(
-                    future.result(), 
-                    event.get('singleResult', event.get('single_result', False))
-                )
-        else:
-            logger.debug('Processing BatchInvoke operation with a batch of {}'.format(len(event)))
-            result = []
-            max_workers = min(len(event), MAX_CONCURRENT_QUERIES)
-            with connection.cursor(max_workers=max_workers) as cursor:
-                futures = []
-                for item in event:
-                    futures.append(cursor.execute(item['query'].format(**item.get('params', {}))[1]))
-                for count in range(0, len(futures)):
-                    # Batch results are always arrays of arrays of maps
-                    result.append(_process_result_set(futures[count].result(), False))
+    if type(event) is dict:
+        logger.info('Processing Invoke operation')
+        with _get_connection(event.get('schemaName')).cursor(max_workers=1) as cursor:
+            _, future = cursor.execute(event['query'].format(**event.get('params', {})))
+            # Default to multiple results if not present
+            result = _process_result_set(
+                future.result(), 
+                event.get('singleResult', event.get('single_result', False))
+            )
+    else:
+        logger.debug('Processing BatchInvoke operation with a batch of {}'.format(len(event)))
+        result = []
+        max_workers = min(len(event), MAX_CONCURRENT_QUERIES)
+        with _get_connection(event.get('schemaName')).cursor(max_workers=max_workers) as cursor:
+            futures = []
+            for item in event:
+                futures.append(cursor.execute(item['query'].format(**item.get('params', {}))[1]))
+            for count in range(0, len(futures)):
+                # Batch results are always arrays of arrays of maps
+                result.append(_process_result_set(futures[count].result(), False))
     return result
 
 
-def _connect(schema_name):
-    return connect(
-        cursor_class=AsyncCursor,
-        poll_interval=float(os.environ.get('POLL_INTERVAL', 1.0)),
-        region_name=os.environ.get('AWS_ATHENA_REGION_NAME', boto3.session.Session().region_name),
-        s3_staging_dir=os.environ['AWS_ATHENA_S3_STAGING_DIR'],
-        schema_name=schema_name if schema_name else os.environ.get('AWS_ATHENA_SCHEMA_NAME', 'default')
-    )
+def _get_connection(schema_name):
+    if schema_name not in CONNECTION_CACHE:
+        CONNECTION_CACHE[schema_name] = connect(
+            cursor_class=AsyncCursor,
+            poll_interval=float(os.environ.get('POLL_INTERVAL', 1.0)),
+            region_name=os.environ.get('AWS_ATHENA_REGION_NAME', boto3.session.Session().region_name),
+            s3_staging_dir=os.environ['AWS_ATHENA_S3_STAGING_DIR'],
+            schema_name=schema_name if schema_name else os.environ.get('AWS_ATHENA_SCHEMA_NAME', 'default')
+        )
+    return CONNECTION_CACHE[schema_name]    
 
 
 def _map_result(description, row):
